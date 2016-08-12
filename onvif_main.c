2540f7320f0f5d9681607893cc024be1ac56ea92
lindaoge <lindaoge_2005@126.com>
2016-08-12 10:46:22 +0800

    code add
    code add by linjiandao 2016-08-12 10:45

--- onvif_main.c
+++ onvif_main.c
@@ -1,20 +1,91 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include "libLog.h"
 
 #include "libOnvifAccess.h"
+#include "libCameraAccess.h"
 
 int main(int argc,char ** argv)
 {
-	printf("%s %d: Common_Init!\n");
-	printf("Onvif starting...\n");
+/*
+    Onvif_Start_All();
+    while(1) sleep(10);
+    return 0;
+*/
 
-	printf("Now, starting the probe thread...\n");
+	int cnt = 0;
+	unsigned short port_webserver = 0;
+	char dev_name[64] = {0};
+
+	port_webserver = DEFAULT_PORT;
+	strcpy(dev_name, DEFAULT_DEVNAME);
+
+    	//CameraDeviceInit(CAMERA_DEV_BS_SD_DOME);
+    	CameraDeviceInit(CAMERA_DEV_COMMON);
+	for(cnt = 1; cnt < argc; cnt++)
+	{
+		if(strcasecmp(argv[cnt], "-p") == 0)
+		{
+			if(cnt+1 < argc)
+			{
+				int port = atoi(argv[cnt+1]);
+				if(port == 0 || port > 65535)
+				{
+					goto Usage;
+				}
+				LogTrace("Port = %d\n", port);
+				port_webserver = port;
+				cnt++;
+			}
+			else
+			{
+				goto Usage;
+			}
+
+		}
+		else if(strcasecmp(argv[cnt], "-i") == 0)
+		{
+			if(cnt+1 < argc)
+			{
+				LogTrace("dev = %s\n", argv[cnt+1]);
+				strcpy(dev_name, argv[cnt+1]);
+				cnt++;
+			}
+			else
+			{
+				goto Usage;
+			}
+
+		}
+
+	}
+
+	LogTrace("Onvif starting...\n");
+	Onvif_Env_Init(port_webserver, dev_name);
+
+	LogTrace("Now, Init config...\n");
+	Onvif_Config_Init();
+
+
+    	LogTrace("Now, starting the webservice thread...\n");
+	Onvif_Start_WebService();
+
+	sleep(1);
+
+	LogTrace("Now, starting the probe thread...\n");
 	Onvif_Start_Probe();
 
-	printf("Now, starting the webservice thread...\n");
-	Onvif_Start_WebService();	
+	sleep(1);
+	LogTrace("Now, starting the event thread...\n");
+	Onvif_Start_EventService();
 
+	while(1) sleep(100);
 
-	while(1);
+Usage:
+	LogError("Usage: %s [-p webserver port] [-i net interface like \"eth0\"...]\n", argv[0]);
+
 	return 0;
 
 
